ce63cbf35ea894b78bd1d1e01566d001ed60e6bf
nenitaE <nenitaespinosa@gmail.com>
2021-06-26 13:57:51 -0400

    update

--- 19-Blockchain-Python/3/Activities/04-Processing_Shell_Output/Unsolved/wallet.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import json
-
-command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
-
-# @TODO: call hd-wallet-derive from a subprocess, store stdin in a variable, and print it
